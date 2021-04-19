3b149d69e1aec342e7dc833375ca14930d465d91
dinojl <74159424+dinojl@users.noreply.github.com>
2021-04-19 12:49:32 -0400

    Init

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		// Window name
+		sAppName = "Shitty Tetris";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		return true;
+	}
+};
+
+int main()
+{
+	Example demo;
+	if (demo.Construct(720, 480, 2, 2))
+		demo.Start();
+	return 0;
+}
